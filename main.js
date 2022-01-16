0cd847a4d8e3fccf4dfcd365515d4dff0ba259a9
Paul <paul.libon@edu.devinci.fr>
2022-01-16 21:15:23 +0100

    Portfolio 2.0

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
+const parallax = document.querySelector('.parallax');
+
+window.addEventListener('scroll', () => {
+  parallax.style.backgroundPositionY = -window.scrollY / 4 + "px";
+})
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
+
+const btnToggle = document.querySelector('.btn-toggle');
+
+
+btnToggle.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+        btnToggle.innerHTML = "Dark"
+
+    } else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+        btnToggle.innerHTML = "Light"
+
+    }
+
+})
